69f9fb4f86c92df26db6277071de550807a9c015
Shane Dombowsky <thebigdombowsky@gmail.com>
2022-07-27 11:56:13 -0600

    Add tests for display username or first and last name

--- src/tests/e2e/e2e-login.spec.ts
+++ src/tests/e2e/e2e-login.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 import { loadAraratHomePage } from '../../../helpers'
 
-test.describe('Login/Logout flow', () => {
+test.describe.parallel('Login/Logout flow', () => {
 
     test.beforeEach(async ({ page }) => {
 
@@ -25,9 +25,6 @@ test.describe('Login/Logout flow', () => {
         await page.locator('input[name="password"]').fill('password')
         await page.locator('input:has-text("Sign In")').click()
 
-        const overviewText = await page.locator('h1:has-text("Overview")')
-        await expect(overviewText).toBeVisible()
-
         await page.locator('#profile-menu-button').click();
         await page.locator('text=Logout').click();
         const logoutText = await page.locator('input:has-text("Sign In")')
@@ -35,4 +32,51 @@ test.describe('Login/Logout flow', () => {
 
     })
 
+    test('Display username when no last name populated in Keycloak', async ({ page }) => {
+
+        await page.locator('input[name="username"]').fill('FirstNameOnly')
+        await page.locator('input[name="password"]').fill('max')
+        await page.locator('input:has-text("Sign In")').click()
+
+        await page.locator('#profile-menu-button').click();
+        const displayName = await page.locator('<h3>First undefined</h3>')
+        await expect(displayName).toContainText('FirstNameOnly')
+
+        await page.locator('#profile-menu-button').click();
+        await page.locator('text=Logout').click();
+        const logoutText = await page.locator('input:has-text("Sign In")')
+        await expect(logoutText).toBeVisible()
+    })
+
+    test('Display username when no first name populated in Keycloak', async ({ page }) => {
+
+        await page.locator('input[name="username"]').fill('LastNameOnly')
+        await page.locator('input[name="password"]').fill('max')
+        await page.locator('input:has-text("Sign In")').click()
+
+        await page.locator('#profile-menu-button').click();
+        const displayName = await page.locator('<h3>Last undefined</h3>')
+        await expect(displayName).toContainText('LastNameOnly')
+
+        await page.locator('#profile-menu-button').click();
+        await page.locator('text=Logout').click();
+        const logoutText = await page.locator('input:has-text("Sign In")')
+        await expect(logoutText).toBeVisible()
+    })
+
+    test('Display username when no first and last name populated in Keycloak', async ({ page }) => {
+
+        await page.locator('input[name="username"]').fill('NoNames')
+        await page.locator('input[name="password"]').fill('max')
+        await page.locator('input:has-text("Sign In")').click()
+
+        await page.locator('#profile-menu-button').click();
+        const displayName = await page.locator('<h3>undefined undefined</h3>')
+        await expect(displayName).toContainText('NoNames')
+
+        await page.locator('#profile-menu-button').click();
+        await page.locator('text=Logout').click();
+        const logoutText = await page.locator('input:has-text("Sign In")')
+        await expect(logoutText).toBeVisible()
+    })
 })
