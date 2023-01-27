0973102e40ecb5451f7958d7829c0e8009f0da1e
Shane Dombowsky <thebigdombowsky@gmail.com>
2023-01-27 10:14:58 -0700

    Clean up debug logging

--- src/tests/e2e/api/notification2.spec.ts
+++ src/tests/e2e/api/notification2.spec.ts
@@ -8,6 +8,5 @@ test("Update notification2 preference via api", async ({ request }) => {
     const preference = new Preferences(request)
 
     const result = await preference.preferencePut( preference.reqPut, 200)
-    console.log(result)
 
 })
