7648dbcef4e62ce6c91680e4348eb00066f85de0
Shane Dombowsky <thebigdombowsky@gmail.com>
2023-02-01 14:33:06 -0700

    Clean up locators for create manual orders

--- src/page-objects/CreateManualOrderPage.ts
+++ src/page-objects/CreateManualOrderPage.ts
@@ -21,9 +21,9 @@ export class CreateManualOrderPage {
     this.manualOrderLocationDropdown = page.getByText('Type any part of location\'s name')
     this.manualOrderLocationSelection = page.getByRole('option', { name: 'Nurse Station 2' })
     this.manualOrderNextButton = page.getByRole('button', { name: '.cls-1{fill:#26193c;} Next' }).first()
-    this.manualOrderSelectMedication = page.getByRole('row', { name: 'Metoprolol Succinate ER 50 CAPSULE SWLG7000 Not a Hazard 9 9 .cls-1{fill:#26193c;} .cls-1{fill:#26193c;} Add' }).locator('#undefined-plus-icon')
-    this.manualOrderIncreaseQuantity = page.getByRole('cell', { name: 'Load Units Lot Expiry Date UPC Qty On Hand Qty Available Qty To Order BST-1 9/24/25 04150010165179 9 9 .cls-1{fill:#26193c;} .cls-1{fill:#26193c;} Add' }).locator('#quantityToOrder-numberPickerPlusIcon')
-    this.manualOrderAddQuantity = page.getByRole('cell', { name: 'Load Units Lot Expiry Date UPC Qty On Hand Qty Available Qty To Order BST-1 9/24/25 04150010165179 9 9 .cls-1{fill:#26193c;} .cls-1{fill:#26193c;} Add' }).getByRole('button', { name: 'Add' })
+    this.manualOrderSelectMedication = page.getByRole('row', { name: 'Metoprolol Succinate ER 50 CAPSULE SWLG7000 Not a Hazard 325 325 .cls-1{fill:#26193c;} .cls-1{fill:#26193c;} Add' }).locator('#undefined-plus-icon')
+    this.manualOrderIncreaseQuantity = page.getByRole('cell', { name: 'Load Units Lot Expiry Date UPC Qty On Hand Qty Available Qty To Order BST-1 9/24/25 04150010165179 9 9 .cls-1{fill:#26193c;} .cls-1{fill:#26193c;} Add Lot1 12/31/24 Swisslog Rossa 2020 316 316 .cls-1{fill:#26193c;} .cls-1{fill:#26193c;} Add' }).getByRole('row', { name: 'BST-1 9/24/25 04150010165179 9 9 .cls-1{fill:#26193c;} .cls-1{fill:#26193c;} Add' }).locator('#quantityToOrder-numberPickerPlusIcon')
+    this.manualOrderAddQuantity = page.getByRole('cell', { name: 'Load Units Lot Expiry Date UPC Qty On Hand Qty Available Qty To Order BST-1 9/24/25 04150010165179 9 9 .cls-1{fill:#26193c;} .cls-1{fill:#26193c;} Add Lot1 12/31/24 Swisslog Rossa 2020 316 316 .cls-1{fill:#26193c;} .cls-1{fill:#26193c;} Add' }).getByRole('row', { name: 'BST-1 9/24/25 04150010165179 9 9 .cls-1{fill:#26193c;} .cls-1{fill:#26193c;} Add' }).getByRole('button', { name: 'Add' })
     this.manualOrderStartUnloadingButton = page.getByRole('button', { name: '.cls-1{fill:#26193c;} Start Unloading' }).first()
 
   }
